8deaa501145c2f7243bb9b1c7472d44c85e4bf1e
Mike Newell <mike_newell@gspsf.com>
2015-06-14 22:28:38 -0700

    added tracking and new resume

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
             <footer class="row">
                 <p class="col-md-4">&copy; ERINN BUTULIS 2015</p>
                 <nav class="col-md-4 col-md-offset-4">
-                    <a href="http://erinnbutulis.com/wp-content/uploads/2015/05/Butulis_FitBit_Resume_Print.pdf"><i class="fa fa-file-pdf-o"></i></a>
+                    <a href="http://erinnbutulis.com/wp-content/uploads/2015/06/Butulis_Resume.pdf"><i class="fa fa-file-pdf-o"></i></a>
                     <a href="https://www.pinterest.com/ebutulis/"><i class="fa fa-pinterest"></i></a>
                     <a href="https://www.linkedin.com/in/ebutulis"><i class="fa fa-linkedin"></i></a>
                 </nav>
